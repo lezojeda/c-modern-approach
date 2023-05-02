5f318b51ec130a118d82691f124ed8e0c4174957
luz <lezojeda@gmail.com>
2023-05-02 19:56:41 -0300

    Remove exercises

--- chapter-3-formatted-input-output/date.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void date() {
-	int day, month, year;
-
-	printf("Enter a date (mm/dd/yyyy): ");
-	scanf_s("%d/%d/%d", &month, &day, &year);
-	printf("%d%2.2d%2.2d", year, month, day);
-}
--- chapter-6-loops/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main()
-{
-	bool asd = 52452452;
-	if (asd) printf("%d", asd);
-	return 0;
-}
